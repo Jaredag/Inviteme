c50133a4c67f30cf95cedcb86c7baf5e1bdb8b8c
mthompson17 <tho16003@byui.edu>
2017-06-07 14:32:15 -0600

    Unit test

--- app/src/test/java/com/me/invite/inviteme/ExampleUnitTest.java
+++ app/src/test/java/com/me/invite/inviteme/ExampleUnitTest.java
@@ -1,9 +1,9 @@
 package com.me.invite.inviteme;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +14,29 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void userConstructorShouldWork(){
+        User user = new User("Michael", "100");
+        String name = user.getUserName();
+        String number = user.getPhoneNumber();
+        assertEquals(name, "Michael");
+        assertEquals(number, "100");
+    }
+
+    @Test
+    public void eventConstructorShouldWork(){
+        Event event = new Event("Mario Party...Party?");
+        String title = event.getTitle();
+        assertEquals(title, "Mario Party...Party?");
+    }
+
+    @Test
+    public void eventDefaultConstructor(){
+        Event event = new Event();
+
+        assertEquals(event.getTitle(), "Enter Title");
+    }
+
+
 }
