e563fc50eabdbbce65a6373205342a2476fab738
Jaredag <jaredg-93@hotmail.com>
2017-06-12 09:59:02 -0600

    No changes thus far

--- app/src/main/java/com/me/invite/inviteme/MainActivity.java
+++ app/src/main/java/com/me/invite/inviteme/MainActivity.java
@@ -1,16 +1,24 @@
 package com.me.invite.inviteme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    //public static final String EXTRA_MESSAGE = "com.me.invite.inviteme.MESSAGE";
     // Comments gone
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Continue as Guest to the Main feed
+    public void continueAsGuest(View view){
+        // Go to the Main Feed
+        Intent intent = new Intent(this, MainFeedActivity.class);
+        startActivity(intent);
     }
 
 }
