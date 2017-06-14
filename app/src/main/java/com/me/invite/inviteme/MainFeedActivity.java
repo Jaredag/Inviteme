08eb7793e2f5d626f390a5e99dbdb579eca22e9d
Evillibrarians <danielstepp25@gmail.com>
2017-06-14 13:27:46 -0600

    Changed from scroll view to list view

--- app/src/main/java/com/me/invite/inviteme/MainFeedActivity.java
+++ app/src/main/java/com/me/invite/inviteme/MainFeedActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
+import static com.me.invite.inviteme.R.id.scrollView;
+
 public class MainFeedActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +16,17 @@ public class MainFeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_feed);
         // stuff more changes
+
+        // Read in events from database
+        DatabaseStorage dbstorage = new DatabaseStorage();
+
+        // Pull array of events from database
+        ArrayList<Shindig> shindigs = new ArrayList<Shindig>();
+        shindigs = dbstorage.getShindigs();
+
+        // Display the events in the correct format in the list
+
+        //
     }
 
     /*public void createShindig(View view) {
