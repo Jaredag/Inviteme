cc197adeae0b2edf36921a4215989cec56db8380
Jaredag <jaredg-93@hotmail.com>
2017-06-17 16:35:47 -0600

    Added more to the function for the onCreate Event Class

--- app/src/main/java/com/me/invite/inviteme/MainFeedActivity.java
+++ app/src/main/java/com/me/invite/inviteme/MainFeedActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.gson.Gson;
@@ -14,12 +15,18 @@ public class MainFeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_feed);
-        // stuff more changes
+
+        SharedPreferences mPrefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = mPrefs.getString(PREFS_NAME, "");
+        Shindig myShindig = gson.fromJson(json, Shindig.class);
+        String message = "This is a title: " + myShindig.getTitle() + " Desc: " + myShindig.getDescription();
+        Log.d("Tag", message);
+
     }
 
-    // Continue as Guest to the Main feed
     public void createAnEvent(View view){
-        // Create an Eventf
+        // Create an Event
         Intent intent = new Intent(this, CreateShindigActivity.class);
         startActivity(intent);
     }
@@ -28,28 +35,4 @@ public class MainFeedActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MyShindigs.class);
         startActivity(intent);
     }
-
-    // This will be used for the myEventsActivity
-    // this will not be a function though, just code in the
-    // activity itself in the onCreate
-    public void getYourEvents(View view){
-        SharedPreferences mPrefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        SharedPreferences.Editor editor = mPrefs.edit();
-        Gson gson = new Gson();
-        String json = mPrefs.getString("shindig", "");
-        Shindig myShindigs = gson.fromJson(json, Shindig.class);
-        // Display them on the screen. 
-    }
-
-    public void saveEvent(View view){
-        // Get id of the shindig
-        String shindig = "Event";
-        SharedPreferences mPrefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        SharedPreferences.Editor editor = mPrefs.edit();
-        Gson gson = new Gson();
-        String json = gson.toJson(shindig);
-        editor.putString(shindig, json);
-        editor.commit();
-    }
-
 }
