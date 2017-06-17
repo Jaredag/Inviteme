179ef1309a882b8e07bc5e84dac9179764115756
Evillibrarians <danielstepp25@gmail.com>
2017-06-17 14:56:41 -0600

    Updating fileview

--- app/src/main/java/com/me/invite/inviteme/MyShindigs.java
+++ app/src/main/java/com/me/invite/inviteme/MyShindigs.java
@@ -2,6 +2,8 @@ package com.me.invite.inviteme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MyShindigs extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MyShindigs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_shindigs);
+
+        // ListView 
+        ListView mListView = (ListView) findViewById(R.id.listView2);
+        /*
+        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
+
+        String[] listItems = new String[recipeList.size()];
+
+        for(int i = 0; i < recipeList.size(); i++){
+            Recipe recipe = recipeList.get(i);
+            listItems[i] = recipe.title;
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
+        */
     }
 }
