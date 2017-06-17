626be1781029d3b74b63d53810263e996147493a
Evillibrarians <danielstepp25@gmail.com>
2017-06-17 16:21:26 -0600

    Getting FileView functioning

--- app/src/main/java/com/me/invite/inviteme/MyShindigs.java
+++ app/src/main/java/com/me/invite/inviteme/MyShindigs.java
@@ -1,31 +1,50 @@
 package com.me.invite.inviteme;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class MyShindigs extends AppCompatActivity {
+    public static final String PREFS_NAME = "MyPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_shindigs);
 
-        // ListView 
+        // Access shared preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        //Json json = settings.getString("silentMode", false);
+        Gson gson = new Gson();
+        String json = settings.getString(PREFS_NAME, "");
+        Shindig myShindig = gson.fromJson(json, Shindig.class);
+
+
+        // Setting up the ListView
         ListView mListView = (ListView) findViewById(R.id.listView2);
-        /*
-        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
 
-        String[] listItems = new String[recipeList.size()];
+        final ArrayList<Shindig> shindigList = new ArrayList<Shindig>(); // ONLINE CODE HAD "=SHINDIG.GETRECIPESFROMFILE("FILE.JSON", THIS);"
 
-        for(int i = 0; i < recipeList.size(); i++){
-            Recipe recipe = recipeList.get(i);
-            listItems[i] = recipe.title;
+        String[] listItems = new String[shindigList.size()];
+        //Vector<Shindig> listShindigs = new Vector<Shindig>();
+
+        for (int i = 0; i < 1; i++)//shindigList.size(); i++)
+        {
+            /*Shindig _shindig = shindigList.get(i);
+            listShindigs.add(_shindig);
+            listItems[i] = _shindig.getTitle();*/
+
+            listItems[i] = myShindig.getTitle();
         }
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
         mListView.setAdapter(adapter);
-        */
     }
 }
