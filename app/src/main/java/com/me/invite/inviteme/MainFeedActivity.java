cf730e1507230650d7429457fd75de76d6f64897
Evillibrarians <danielstepp25@gmail.com>
2017-06-17 16:41:04 -0600

    Getting FileView functioning on Main Feed Activity

--- app/src/main/java/com/me/invite/inviteme/MainFeedActivity.java
+++ app/src/main/java/com/me/invite/inviteme/MainFeedActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class MainFeedActivity extends AppCompatActivity {
     public static final String PREFS_NAME = "MyPrefs";
@@ -22,6 +26,29 @@ public class MainFeedActivity extends AppCompatActivity {
         Shindig myShindig = gson.fromJson(json, Shindig.class);
         String message = "This is a title: " + myShindig.getTitle() + " Desc: " + myShindig.getDescription();
         Log.d("Tag", message);
+
+
+        // Setting up the ListView
+        ListView mListView = (ListView) findViewById(R.id.listView2);
+
+        final ArrayList<Shindig> shindigList = new ArrayList<Shindig>(); // ONLINE CODE HAD "=SHINDIG.GETRECIPESFROMFILE("FILE.JSON", THIS);"
+
+        String[] listItems = new String[shindigList.size()];
+        //Vector<Shindig> listShindigs = new Vector<Shindig>();
+
+        for (int i = 0; i < 1; i++)//shindigList.size(); i++)
+        {
+            /*Shindig _shindig = shindigList.get(i);
+            listShindigs.add(_shindig);
+            listItems[i] = _shindig.getTitle();*/
+
+            listItems[i] = myShindig.getTitle();
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
+
+
 
     }
 
